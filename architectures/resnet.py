80f8620e6487ea31d4106da7f073ab7103f297d7
Ansh Singhal <singhal.ansh0503@gmail.com>
2024-02-18 14:41:27 +0530

    updated expansion

--- architectures/resnet.py
+++ architectures/resnet.py
@@ -1,8 +1,9 @@
-import torch
 import torch.nn as nn
 
 
 class BasicBlock(nn.Module):
+    expansion = 1
+
     def __init__(self, in_channels, interm_channels, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=interm_channels, kernel_size=3, stride=stride,
