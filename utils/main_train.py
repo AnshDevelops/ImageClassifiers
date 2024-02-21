22ab1f7023af1fa9c89703a5e0764ca4eb8cb012
Ansh Singhal <singhal.ansh0503@gmail.com>
2024-02-21 18:01:40 +0530

    added main_train

---
+++ utils/main_train.py
@@ -0,0 +1,44 @@
+import torch
+import tqdm.auto as tqdm
+
+from train_step import train_step
+from test_step import test_step
+
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          device: torch.device,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = torch.nn.CrossEntropyLoss,
+          epochs: int = 5):
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []
+               }
+
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer,
+                                           device=device)
+        test_loss, test_acc = test_step(model=model,
+                                        dataloader=test_dataloader,
+                                        loss_fn=loss_fn,
+                                        device=device)
+
+        print(
+            f"Epoch: {epoch} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f} | "
+        )
+
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    return results
